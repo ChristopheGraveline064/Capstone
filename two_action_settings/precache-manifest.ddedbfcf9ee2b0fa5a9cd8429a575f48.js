b17ae6981b208df09fbd9ef085c8c68ececbe2a0
ChristopheGraveline064 <cgrav064@uottawa.ca>
2019-03-20 18:34:52 -0400

    Add files via upload

---
+++ two_action_settings/precache-manifest.ddedbfcf9ee2b0fa5a9cd8429a575f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c0b92d2210ffd5c315b8bed906cbd3",
+    "url": "/jmorandf/two_action_settings/static/media/Birdwave.78c0b92d.jpg"
+  },
+  {
+    "revision": "8dfa9083a48a8e5c616a",
+    "url": "/jmorandf/two_action_settings/static/js/runtime~main.8dfa9083.js"
+  },
+  {
+    "revision": "5ca43b011cd8fd20c04f",
+    "url": "/jmorandf/two_action_settings/static/js/main.5ca43b01.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/jmorandf/two_action_settings/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "5ca43b011cd8fd20c04f",
+    "url": "/jmorandf/two_action_settings/static/css/main.c5dcf35f.chunk.css"
+  },
+  {
+    "revision": "1dc57e2e21ab7179ee3c0da4bb873ef4",
+    "url": "/jmorandf/two_action_settings/index.html"
+  }
+];
---
+++ two_action_settings/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/jmorandf/two_action_settings/precache-manifest.ddedbfcf9ee2b0fa5a9cd8429a575f48.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/jmorandf/two_action_settings/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
